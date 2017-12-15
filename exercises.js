305353bc2dbd1efd995492084e18d4c13ca93255
Brandon Fowler <bmfowler4@gmail.com>
2017-12-14 22:14:49 -1000

    finished exercises

--- exercises.js
+++ exercises.js
@@ -5,6 +5,12 @@
  * @return {string} reversed
  * ie: "cat" => "tac"
  */
+ var firstReverse = function(str) {
+ 	if (typeof str !== 'string')
+ 		return null;
+
+ 	return str.split('').reverse().join(''); 
+ }
 
  /** Function: alphaOrder
  * The function will take the str parameter being passed in and
@@ -13,6 +19,12 @@
  * @return {string} in alphabetical order
  * ie: "cake" => "acek"
  */
+ var alphaOrder = function(str) {
+ 	if (typeof str !== 'string')
+ 		return null;
+
+ 	return str.split('').sort().join('');
+ }
 
  /** Function: vowelCount
  * The function will take the str parameter being passed in and
@@ -21,6 +33,18 @@
  * @return {number} count of vowels
  * ie: "oreo" => 3
  */
+ var vowelCount = function(str) {
+ 	if (typeof str !== 'string')
+ 		return null;
+
+ 	var obj = {'a': 0, 'e': 0, 'i': 0, 'o': 0, 'u': 0};
+ 	var res = 0;
+ 	for (var i = 0; i < str.length; i ++)
+ 		if (obj.hasOwnProperty(str.charAt(i)))
+ 			res ++;
+
+ 	return res;
+ }
 
  /** Function: timeConvert
  * The function will take the str parameter representing the amount of minutes being passed in and
@@ -30,6 +54,12 @@
  * @return {string} as hours:minutes
  * ie: 68 => 1:8
  */
+ var timeConvert = function(str) {
+ 	if (typeof str !== 'number')
+ 		return null;
+
+ 	return Math.floor(str / 60) + ':' + str % 60;
+ }
 
  /** Function: repeatString
  * The function will take in two parameters and repeat a given string (first argument)
@@ -39,6 +69,19 @@
  * @return {string} repeated num times
  * i.e repeatString("money", 3) => "moneymoneymoney".
  */
+ var repeatString = function(str, num) {
+ 	if (typeof str !== 'string' || typeof num !== 'number')
+ 		return null;
+ 	
+ 	if (num < 0)
+ 		return '';
+
+ 	var res = '';
+ 	for (var i = 1; i <= num; i ++)
+ 		res += str;
+
+ 	return res;
+ }
 
 
 /**
@@ -56,9 +99,9 @@
  */
 
 module.exports = {
-    firstReverse: null,
-    alphaOrder: null,
-    vowelCount: null,
-    timeConvert: null,
-    repeatString: null
+    firstReverse: firstReverse,
+    alphaOrder: alphaOrder,
+    vowelCount: vowelCount,
+    timeConvert: timeConvert,
+    repeatString: repeatString
 }
